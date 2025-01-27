e454eb6681d05c57a8b7acd8eefce6df7b4a43f7
Gabriel Coelho <coelhocostag@gmail.com>
2025-01-27 20:55:40 -0300

    server: add povoamento tabelas restantes

--- server/prisma/seeds/index.ts
+++ server/prisma/seeds/index.ts
@@ -3,14 +3,24 @@ import { SalespersonSeed } from './salesperson'
 import { Seed } from './Seed'
 import { CategorySeed } from './category'
 import { ProductSeed } from './product'
+import { ClientSeed } from './client'
+import { CouponSeed } from './coupon'
+import { PaymentMethodSeed } from './payment-method'
+import { SaleSeed } from './sale'
+import { SaleProductsSeed } from './sale-products'
 
 async function seed() {
   const prisma = new PrismaClient()
 
   const seedInstances: Seed[] = [
+    new ClientSeed(prisma),
+    new CouponSeed(prisma),
+    new PaymentMethodSeed(prisma),
     new SalespersonSeed(prisma),
     new CategorySeed(prisma),
     new ProductSeed(prisma),
+    new SaleSeed(prisma),
+    new SaleProductsSeed(prisma),
   ]
 
   for (const seedInstance of seedInstances.slice().reverse())
